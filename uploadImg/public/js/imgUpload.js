4a0bf5e1bd80a9c78da5cc104d5950ad8669dda2
Hoxun <Ho.xun@outlook.com>
2018-11-23 21:24:27 +0800

    优化

--- uploadImg/public/js/imgUpload.js
+++ uploadImg/public/js/imgUpload.js
@@ -1,29 +1,45 @@
 /**
  * [ImgUpload description]
- * @chooseFileButton  element  input[type=file]
- * @showContainer element
- * @submitButton element
+ * @chooseFileButton object element  input[type=file]
+ * @showImgContainer object element
+ * @submitButton object element
+ * @uploadAddress string
  */
-var ImgUpload = {
-	fileInput: null,
-	showBox: null,
-	submitBtn: null,
-	url: "http://localhost/MYPHP/uploadImg/server/index.php",
-	formData: new FormData(),
-	arrFiles: [],
-	_this: null,
-	init: function(chooseFileButton, showContainer, submitButton) {
-		_this = this;
-		this.fileInput = chooseFileButton;
-		this.showBox = showContainer;
-		this.submitBtn = submitButton;
-		chooseFileButton.onchange = function() {
-			_this.showImg();
+(function() {
+	window.imgUpload = imgUpload;
+	function imgUpload(chooseFileButton, showImgContainer, submitButton, uploadAddress) {
+		return new imgUpload.prototype.init(chooseFileButton, showImgContainer, submitButton, uploadAddress);
+	}
+	var formData = new FormData(),
+		arrFiles = [];
+
+	var init = imgUpload.prototype.init = function(fileInput, showBox, submitBtn, url) {
+		if (!fileInput || !showBox || !submitBtn || !url) {
+			return this;
 		}
-		this.submitImg();
-	},
-	showImg: function() {
-		if (!this.fileInput.value) {
+		fileInput.onchange = function() {
+			showImg(fileInput, showBox, submitBtn);
+		};
+		submitBtn.onclick = function() {
+			submitImg(url, resetFb(showBox, submitBtn));
+		};
+	}
+	init.prototype = imgUpload.prototype;
+
+	function resetFb(showBox, submitBtn) {
+		var showBox = showBox,
+			submitBtn = submitBtn;
+		return function(message) {
+			showBox.innerText = '';
+			// arrFiles = [];
+			arrFiles.splice(0, arrFiles.length);
+			formData.delete("Img[]");
+			submitBtn.style.display = 'none';
+		}
+	}
+
+	function showImg(fileInput, showBox, submitBtn) {
+		if (!fileInput.value) {
 			return;
 		}
 		var fragment = document.createDocumentFragment(),
@@ -31,81 +47,31 @@ var ImgUpload = {
 			isImg,
 			imgEl,
 			src,
-			fileLen = this.fileInput.files.length;
+			fileLen = fileInput.files.length;
 
 		for (var i = 0; i < fileLen; i++) {
-			isImg = this.checkImg(this.fileInput.files[i]);
+			isImg = checkImg(fileInput.files[i]);
 			if (!isImg) {
 				continue;
 			}
-			src = window.URL.createObjectURL(this.fileInput.files[i]);
-			imgEl =  this.createImgEl(src);
+			src = window.URL.createObjectURL(fileInput.files[i]);
+			imgEl = createImgEl(src);
 			fragment.appendChild(imgEl);
-
-			arrTmp.push(this.fileInput.files[i]);
-			
+			arrTmp.push(fileInput.files[i]);
+			deleteImg(imgEl, showBox);
 		}
 		// this.showBox.appendChild(fragment);
-		this.showBox.insertBefore(fragment, this.showBox.firstChild);
-
-		this.arrFiles = arrTmp.concat(this.arrFiles );
+		// arrFiles = arrFiles.concat(arrTmp );
+		showBox.insertBefore(fragment, showBox.firstElementChild);
+		arrFiles = arrTmp.concat(arrFiles );
 		// 清空 input 的内容，使得下次选择同一张图片时依然触发 onchange 事件；
-		this.fileInput.value = '';
-		this.submitBtn.style.display = 'inline-block';
+		fileInput.value = '';
+		submitBtn.style.display = 'inline-block';
 
-		
-	},
-	createImgEl: function(src) {
-		var figure,
-			img,
-			delBtn;
-		figure = document.createElement('figure');
-		img = document.createElement('img');
-		img.setAttribute('src', src);
-		figure.appendChild(img);
-
-		delBtn = this.createDelBtn();
-		figure.appendChild(delBtn);
-
-		this.showDelBtn(delBtn);
-		this.deleteImg(delBtn);
-		return figure;
-	},
-	createDelBtn: function() {
-		var figcaption = document.createElement('figcaption');
-		figcaption.classList.add('delete-btn');
-		figcaption.innerText = '删除';
-		figcaption.style.display = 'none';
-		return figcaption;
-	},
-	deleteImg: function(delBtn) {
-		delBtn.onclick = function(e) {
-			for (var i = 0; i < _this.showBox.children.length; i++) {
-				if (_this.showBox.children[i] == e.target.parentElement) {
-					_this.arrFiles.splice(i, 1);
-					continue;
-				}
-			}
-			var elTmp = this.parentNode;
-			elTmp.parentNode.removeChild(elTmp);
-		}
-	},
-	showDelBtn: function(delBtn) {
-		var parentEl = delBtn.parentElement;
-		parentEl.onmouseenter = function() {
-			delBtn.style.display = 'block'
-		}
-		parentEl.onmouseleave = function() {
-			delBtn.style.display = 'none'
-		}
-		delBtn.onmouseenter = function() {
-			delBtn.style.color = '#FF4400'
-		}
-		delBtn.onmouseleave = function() {
-			delBtn.style.color = '#000000'
-		}
-	},
-	checkImg: function(file) {
+	}
+	
+	// 检测是不是图片格式
+	function checkImg(file) {
 		var isImg;
 		if (file.type.indexOf("image") == 0) {
 			if (file.size >= 1024 * 1024) {
@@ -119,42 +85,82 @@ var ImgUpload = {
 			isImg = false;
 		}
 		return isImg;
-	},
-	submitImg: function() {
-		this.submitBtn.onclick = function(event) {
-			var xhr = new XMLHttpRequest(),
-				fileLen = _this.arrFiles.length;
-			if(!fileLen) {
-				return;
-			}
-			xhr.open("POST", _this.url, true);
-			// console.log('OPENED', xhr.readyState);
-			xhr.onreadystatechange = function() {
-				if (xhr.readyState == 4) {
-					if (xhr.status == 200) {
-						var xhrData = xhr.responseText;
-						_this.showBox.innerText = '';
-						// console.log(xhrData);
-						_this.arrFiles = [];
-						_this.arrFiles.splice(0,_this.arrFiles.length);
-						_this.formData.delete("Img[]");
-						_this.submitBtn.style.display = 'none';
-						alert(xhrData);
-					} else {
-						var xhrData = xhr.responseText;
-						console.log(xhrData);
-					}
+	}
+
+	function createImgEl(src) {
+		var figure,
+			img,
+			delBtn;
+		figure = document.createElement('figure');
+		img = document.createElement('img');
+		img.setAttribute('src', src);
+		figure.appendChild(img);
+
+		delBtn = createDelBtn();
+		figure.appendChild(delBtn);
+		return figure;
+	}
+
+	function createDelBtn() {
+		var figcaption = document.createElement('figcaption');
+		figcaption.classList.add('delete-btn');
+		figcaption.innerText = '删除';
+		figcaption.style.display = 'none';
+		return figcaption;
+	}
+
+	function deleteImg(imgEl, showBox) {
+		var delBtn = imgEl.lastElementChild;
+		showDelBtn(delBtn, imgEl);
+		delBtn.onclick = function(e) {
+			var imgLen = showBox.children.length;
+			for (var i = 0; i < imgLen; i++) {
+				if (showBox.children[i] == imgEl) {
+					arrFiles.splice(i, 1);
+					continue;
 				}
-			};
-			for (var i = 0; i < fileLen; i++) {
-				_this.formData.append("Img[]", _this.arrFiles[i]);
 			}
-			// var x = _this.formData.entries();
-			// console.log(x.next());
-			// console.log(x.next());
-			// console.log(x.next());
-			// console.log(x.next());
-			xhr.send(_this.formData);
+			showBox.removeChild(imgEl);
 		}
 	}
-}
+
+	function showDelBtn(delBtn, imgEl) {
+		imgEl.onmouseenter = function() {
+			delBtn.style.display = 'block';
+		}
+		imgEl.onmouseleave = function() {
+			delBtn.style.display = 'none';
+		}
+		delBtn.onmouseenter = function() {
+			delBtn.style.color = '#FF4400';
+		}
+		delBtn.onmouseleave = function() {
+			delBtn.style.color = '#000000';
+		}
+	}
+	function submitImg(url, fn) {
+		var fileLen = arrFiles.length;
+		if(!fileLen) {
+			return;
+		}
+		var xhr = new XMLHttpRequest();
+		xhr.open("POST", url, true);
+		// console.log('OPENED', xhr.readyState);
+		xhr.onreadystatechange = function() {
+			if (xhr.readyState == 4) {
+				if (xhr.status == 200) {
+					var xhrData = xhr.responseText;
+					alert(xhrData);
+					fn(xhrData);
+				} else {
+					var xhrData = xhr.responseText;
+					console.log(xhrData);
+				}
+			}
+		};
+		for (var i = 0; i < fileLen; i++) {
+			formData.append("Img[]", arrFiles[i]);
+		}
+		xhr.send(formData);
+	}
+}());
